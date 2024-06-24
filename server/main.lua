467a743256fabc9c08c4f4cda8c6d242df460cfa
Cs0ng0r <bikadicsongor@gmail.com>
2024-06-24 22:00:52 +0200

    Added license to log

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,10 @@
-lib.callback.register("sharky_crafting:server:CraftItem", function(source, recipe, coords)
+lib.callback.register("HP-Crafting:server:CraftItem", function(source, recipe, coords)
     if exports.ox_inventory:CanCarryItem(source, recipe.item, recipe.amount) then
         for i = 1, #recipe.requiredItems do
             exports.ox_inventory:RemoveItem(source, recipe.requiredItems[i].name, recipe.requiredItems[i].amount)
         end
         exports.ox_inventory:AddItem(source, recipe.item, recipe.amount)
-        sendToDiscord(_U('successful_craft'), _U('craft_format', GetPlayerName(source), recipe.item, recipe.amount, coords), 65280)
+        sendToDiscord(_U('successful_craft'), _U('craft_format', GetPlayerName(source),GetPlayerIdentifierByType(source, 'license'):gsub('license:', ''), recipe.item, recipe.amount, coords), 65280)
     else
         TriggerClientEvent('ox_lib:notify', source, {
             title = _U('notify_title'),
@@ -12,16 +12,16 @@ lib.callback.register("sharky_crafting:server:CraftItem", function(source, recip
             position = "top",
             type = 'error'
         })
-        sendToDiscord(_U('failed_craft'), _U('craft_format', GetPlayerName(source), recipe.item, recipe.amount, coords), 16711680)
+        sendToDiscord(_U('failed_craft'), _U('craft_format', GetPlayerName(source),GetPlayerIdentifierByType(source, 'license'):gsub('license:', '') , recipe.item, recipe.amount, coords), 16711680)
     end
 end)
 
 
-lib.callback.register("sharky_crafting:server:StartedCraft", function(source, recipe, coords)
-    sendToDiscord(_U('craft_started'), _U('craft_format', GetPlayerName(source), recipe.item, recipe.amount, coords),  16776960)
+lib.callback.register("HP-Crafting:server:StartedCraft", function(source, recipe, coords)
+    sendToDiscord(_U('craft_started'), _U('craft_format', GetPlayerName(source), GetPlayerIdentifierByType(source, 'license'):gsub('license:', '') ,recipe.item, recipe.amount, coords),  16776960)
 end)
 
 
-lib.callback.register("sharky_crafting:server:StoppedCraft", function(source, recipe, coords)
-    sendToDiscord(_U('stopped_craft'), _U('craft_format', GetPlayerName(source), recipe.item, recipe.amount, coords), 16711680)
+lib.callback.register("HP-Crafting:server:StoppedCraft", function(source, recipe, coords)
+    sendToDiscord(_U('stopped_craft'), _U('craft_format', GetPlayerName(source),GetPlayerIdentifierByType(source, 'license'):gsub('license:', '') , recipe.item, recipe.amount, coords), 16711680)
 end)
